6f927679f5681c59ca3befdcf3e3bcff13ae6cfa
Vraj Patel <56947880+vrajpatel20@users.noreply.github.com>
2021-01-13 14:58:21 +0530

    Add files via upload

---
+++ tulsionmark/about.php
@@ -0,0 +1,23 @@
+<?php include('include/home/header.php'); ?>
+	
+	<section id="form"><!--form-->
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4 col-sm-offset-1">
+					<div class="login-form"><!--login form-->
+						<h2>About Us</h2>
+						<address>
+	    					<p>Company Name:Tulsionmark</p>
+							<!-- <p>Address:</p> -->
+							<p>Mobile:9328006926</p>
+							<!-- <p>Fax:</p> -->
+							<p>Email:tulsionmark@gmail.com </p>
+	    				</address>
+					</div><!--/login form-->
+				</div>
+			</div>
+		</div>
+	</section><!--/form-->
+	
+	
+<?php include('include/home/footer.php'); ?>
---
+++ tulsionmark/include/home/footer.php
@@ -0,0 +1,15 @@
+<div class="footer-bottom">
+			<div class="container">
+				<div class="row"style="display: flex;justify-content: center;">
+					<p class="pull-left">Copyright © 2021 Tulsionmark. All rights reserved.</p>
+					<!-- <p class="pull-right">Designed by <span>Tulsionmark Tech Team</span></p> -->
+				</div>
+			</div>
+		</div>
+						
+    <script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
